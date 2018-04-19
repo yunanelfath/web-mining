c123581380420d04750b6aa95e8a8f285dea195d
Yunan <yunanelfath@gmail.com>
2018-04-19 19:52:17 +0700

    fix sastrawi version

---
+++ stemmer/index.php
@@ -0,0 +1,19 @@
+<?php
+// demo.php
+
+// include composer autoloader
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// create stemmer
+// cukup dijalankan sekali saja, biasanya didaftarkan di service container
+$stemmerFactory = new \Sastrawi\Stemmer\StemmerFactory();
+$stemmer  = $stemmerFactory->createStemmer();
+
+// stem
+$sentence = 'Perekonomian Indonesia sedang dalam pertumbuhan yang membanggakan';
+$output   = $stemmer->stem($sentence);
+
+echo $output . "\n<br/>";
+// ekonomi indonesia sedang dalam tumbuh yang bangga
+
+echo $stemmer->stem('Mereka meniru-nirukannya') . "\n";
