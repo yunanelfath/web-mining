212bcde8e70669b6dd646ad34dea212a375e27dc
yunanelfath <yunanelfath@ymail.com>
2018-06-26 10:03:59 +0700

    how to call api
    calling api via ajax is absolutely wrong, then call it through server side instead

--- stemmer/index.php
+++ stemmer/index.php
@@ -13,7 +13,20 @@ $stemmer  = $stemmerFactory->createStemmer();
 $sentence = 'Perekonomian Indonesia sedang dalam pertumbuhan yang membanggakan';
 $output   = $stemmer->stem($sentence);
 
-echo $output . "\n<br/>";
+//echo $output . "\n<br/>";
 // ekonomi indonesia sedang dalam tumbuh yang bangga
 
-echo $stemmer->stem('Mereka meniru-nirukannya') . "\n";
+//echo $stemmer->stem('Mereka meniru-nirukannya') . "\n";
+
+$http = new \GuzzleHttp\Client();
+
+$main_url = "https://sandbox-id.xfers.com/api/v3";
+
+$headers = ['X-XFERS-USER-API-KEY'=> "ebzFVj2PXd-ov1dH3o6xAxLqstcRqMJ7gsY3K98qu8o"];
+
+$response = $http->get($main_url."/user", ['headers'=> $headers]);
+
+$response = json_decode($response->getBody()->getContents(), true);
+
+print_r($response);
+
